4d12807742dc05647657c76389ef227e5a99a185
Parker Kulavic <pkulavivc1@gmail.com>
2022-11-01 10:51:30 -0700

    sudoku

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for, render_template
+from engine import SudokuBoard
 
 app = Flask(__name__)
 
@@ -15,5 +16,25 @@ def parker_official():
     return home()
 
 def home():
-    return render_template("home.html")
+    return render_template("home.jinja")
 
+@app.route('/parker/sudoku')
+def parker_sudoku():
+    return sudoku()
+
+def sudoku(difficulty="hard"):
+    b = SudokuBoard()
+    b.fill_puzzle()
+    if difficulty == "easy":
+        b.remove_clues(50)
+    elif difficulty == "hard":
+        b.remove_clues(60)
+    return render_template("sudoku.jinja", game=b, difficulty=difficulty)
+
+@app.route('/parker/sudoku/hard')
+def sudoku_hard():
+    return redirect(url_for('parker_sudoku'))
+
+@app.route('/parker/sudoku/easy')
+def sudoku_easy():
+    return sudoku(difficulty="easy")
